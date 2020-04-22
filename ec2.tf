0e13361115dc71df38ad8dbf5375dfb23e788bb5
satish757 <62093291+satish757@users.noreply.github.com>
2020-04-22 15:35:41 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -21,7 +21,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = ami-07ebfd5b3428b6f4d
+  ami           = "ami-07ebfd5b3428b6f4d"
   instance_type = "t2.micro"
 
   tags {
